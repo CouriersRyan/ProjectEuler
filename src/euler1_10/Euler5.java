d01dcab52353b7f78ee672bd55fe31bff989bd63
CouriersRyan <zhangryan09@gmail.com>
2017-07-13 23:12:12 -0700

    Fifth Commit

---
+++ src/euler1_10/Euler5.java
@@ -0,0 +1,19 @@
+package euler1_10;
+
+import java.util.stream.*;
+
+public class Euler5 {
+	public static void main(String args[]){
+		boolean temp = false;
+		int n = 0;
+		while(!temp){
+			n++;
+			temp = true;
+			for(int i = 1; i < 20; i++){
+				temp = temp && (n % i == 0);
+			}
+		}
+		System.out.println(n);
+	}
+
+}
